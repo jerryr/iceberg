a80b9ce4807a1b22d6907fa3977f9962892f0c0c
Jerry Raj <code@door21.net>
2016-04-26 14:18:46 +0530

    Detect iceberg tags from Docker labels

--- iceberg.go
+++ iceberg.go
@@ -6,6 +6,7 @@ import (
 	"github.com/docker/engine-api/types"
 	"golang.org/x/net/context"
 	"time"
+	"strconv"
 )
 func main() {
 	fmt.Println("Starting Iceberg...")
@@ -13,19 +14,20 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	services := make(map[string] Service)
+	services := make(map[string] *Service)
 	timer := time.NewTicker(time.Second * 5)
+	updateServices(cli, services)
 	go func() {
 		for _ = range timer.C {
 			updateServices(cli, services)
 		}
 	}()
 	select {
-	
+
 	}
 }
 
-func updateServices(cli *client.Client, services map[string] Service) {
+func updateServices(cli *client.Client, services map[string] *Service) {
 	fmt.Println("Updating...")
 	options := types.ContainerListOptions{All: true}
 	containers, err := cli.ContainerList(context.Background(), options)
@@ -34,6 +36,37 @@ func updateServices(cli *client.Client, services map[string] Service) {
 	}
 
 	for _, c := range containers {
-		fmt.Println(c.ID)
+		labels := c.Labels
+		svcname := labels["com.docker.compose.service"]
+		svc, ok := services[svcname]
+		if ! ok {
+			svc = NewService(svcname)
+			services[svcname] = svc
+		}
+		var str string
+		str, ok = labels["iceberg.minimum_count"]
+		if ! ok {
+			fmt.Println("No value given for minumum count!!")
+		} else {
+			var min int64
+			min, err = strconv.ParseInt(str, 10, 64)
+			if err != nil {
+				fmt.Printf("Could not parse %v into a float\n", str)
+			} else {
+				svc.min = min
+			}
+		}
+		str, ok = labels["iceberg.kill_probability"]
+		if ! ok {
+			fmt.Println("No value given for kill probability!!")
+		} else {
+			var kp float64
+			kp, err = strconv.ParseFloat(str, 64)
+			if err != nil {
+				fmt.Printf("Could not parse %v into an int\n", str)
+			} else {
+				svc.killProb = kp
+			}
+		}
 	}
 }
--- service.go
+++ service.go
@@ -3,6 +3,10 @@ package main
 type Service struct {
 	id string
 	min int64
-	killProb float32
+	killProb float64
+	running map[string] bool
 }
 
+func NewService(svcname string) *Service {
+	return &Service{id: svcname, running: make(map[string] bool)}
+}
